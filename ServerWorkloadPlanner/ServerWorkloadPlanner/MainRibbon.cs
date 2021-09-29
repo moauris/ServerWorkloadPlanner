7a07c356b830f9b1e6bf71f0681ac39661db6965
Mo Chen <mo.chen@kyndryl.com>
2021-09-29 11:04:17 +0800

    Initial Commit

---
+++ ServerWorkloadPlanner/ServerWorkloadPlanner/MainRibbon.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerWorkloadPlanner
+{
+    public partial class MainRibbon
+    {
+        private void MainRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void toggleButton1_Click(object sender, RibbonControlEventArgs e)
+        {
+            
+        }
+    }
+}
